b19b9d4c47abb97ffa4ea7fc9e44d8e2bd5e5503
zhangjie06 <19834290400@163.com>
2026-09-08 20:41:53 +0800

    0908

---
+++ 0908/1.js
@@ -0,0 +1,16 @@
+//二叉树的中序遍历
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = []
+    const inorder = (root) => {
+        if(!root) return
+        inorder(root.left)
+        res.push(root.val)
+        inorder(root.right)
+    }
+    inorder(root)
+    return res
+};

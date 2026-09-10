76144316d668a571c1e74550ab7cc135bf6940f7
zhangjie06 <19834290400@163.com>
2026-09-10 21:22:29 +0800

    0910  二叉树最大深度

---
+++ 0910/1.js
@@ -0,0 +1,14 @@
+//104 二叉树的最大深度
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root){
+        return 0
+    }else{
+        const left = maxDepth(root.left)
+        const right = maxDepth(root.right)
+        return Math.max(left,right)+1
+    }
+};
